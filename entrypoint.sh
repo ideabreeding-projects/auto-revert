2a93ae0b66a3466f0723868a29e4c611bada7d32
hyseo492 <41456138+hyseo492@users.noreply.github.com>
2024-11-05 13:10:07 +0900

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "Set the GITHUB_TOKEN env variable."
+	exit 1
+fi
+
+URI=https://api.github.com
+API_HEADER="Accept: application/vnd.github.v3+json"
+AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
+
+pr_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
+          "${URI}/repos/$REPO_FULLNAME/pulls/$PR_NUMBER")
+
+HEAD_REPO=$(echo "$pr_resp" | jq -r .head.repo.full_name)
+HEAD_BRANCH=$(echo "$pr_resp" | jq -r .head.ref)
+
+git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+git config --global user.email "revert@github.com"
+git config --global user.name "GitHub Revert Action"
+
+set -o xtrace
+
+git fetch origin $HEAD_BRANCH
+
+# do the revert
+git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
+
+# check commit exists
+git cat-file -t $COMMIT_TO_REVERT
+git revert $COMMIT_TO_REVERT --no-edit
+git push origin $HEAD_BRANCH
