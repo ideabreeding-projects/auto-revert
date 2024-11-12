fd9d5750f640e7a5d5448fa1911ef288e3b3c2a9
hyseo492 <41456138+hyseo492@users.noreply.github.com>
2024-11-12 10:41:51 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,28 +7,6 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
 	exit 1
 fi
 
-URI=https://api.github.com
-API_HEADER="Accept: application/vnd.github.v3+json"
-AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
+REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 
-pr_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
-          "${URI}/repos/$REPO_FULLNAME/pulls/$PR_NUMBER")
-
-HEAD_REPO=$(echo "$pr_resp" | jq -r .head.repo.full_name)
-HEAD_BRANCH=$(echo "$pr_resp" | jq -r .head.ref)
-
-git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
-git config --global user.email "revert@github.com"
-git config --global user.name "GitHub Revert Action"
-
-set -o xtrace
-
-git fetch origin $HEAD_BRANCH
-
-# do the revert
-git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
-
-# check commit exists
-git cat-file -t $COMMIT_TO_REVERT
-git revert $COMMIT_TO_REVERT --no-edit
-git push origin $HEAD_BRANCH
+git revert HEAD
