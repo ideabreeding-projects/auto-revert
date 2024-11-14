9177c5201a9e37345675ff89f88b19683d11b91c
hyseo492 <41456138+hyseo492@users.noreply.github.com>
2024-11-14 10:29:54 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,11 +15,13 @@ AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 
 commit_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/$REPO_PULLNAME/commits/main)
 
-PARENT_SHA=$(echo "$commit_resp" | jq -r .parents.[].sha)
+PARENT_SHA=$(echo "$commit_resp" | jq "-r .parents.[].sha")
 
 git config --global --add safe.directory /github/workspace
 
 git config --global user.email "hyseo@ymtech.co.kr"
+
+git config --global user.name "hyseo492"
 
 git revert $PARENT_SHA
 
