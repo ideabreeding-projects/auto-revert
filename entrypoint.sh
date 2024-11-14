91162a45571a75e0caf42bddb43f4fd86bda3893
hyseo492 <41456138+hyseo492@users.noreply.github.com>
2024-11-14 10:33:45 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ URI=https://api.github.com
 API_HEADER="X-GitHub-Api-Version: 2022-11-28"
 AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 
-commit_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/$REPO_PULLNAME/commits/main)
+commit_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/$REPO_PULLNAME/commits/main")
 
 PARENT_SHA=$(echo "$commit_resp" | jq "-r .parents.[].sha")
 
