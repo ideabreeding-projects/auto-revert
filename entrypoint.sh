16e896043735dd5a946705e59308c0c0dff01ef7
hyseo492 <41456138+hyseo492@users.noreply.github.com>
2024-11-14 11:20:03 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ URI=https://api.github.com
 API_HEADER="X-GitHub-Api-Version: 2022-11-28"
 AUTH_HEADER="Authorization: Bearer $GITHUB_TOKEN"
 
-echo "API = "$URI/repos/$REPO_PULLNAME/commits/main"
+echo "API = $URI/repos/$REPO_PULLNAME/commits/main"
 
 commit_resp=$(curl -s -H "$AUTH_HEADER" -H "$API_HEADER" "$URI/repos/$REPO_PULLNAME/commits/main")
 
