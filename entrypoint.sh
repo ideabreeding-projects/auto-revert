bbc6cc0b8aab33b396f7f14cb866d206f1a519ac
hyseo492 <41456138+hyseo492@users.noreply.github.com>
2024-11-14 09:46:12 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,21 @@ fi
 
 REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 
+URI=https://api.github.com
+API_HEADER="X-GitHub-Api-Version: 2022-11-28"
+AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
+
+commit_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/$REPO_PULLNAME/commits/main)
+
+PARENT_SHA=$(echo "$commit_resp" | jq -r '.parents | .[].sha')
+
+
 git config --global --add safe.directory /github/workspace
 
 git config --global user.email "hyseo@ymtech.co.kr"
 
 git config --global user.name "hyseo492"
 
-git revert HEAD
+git revert $PARENT_SHA
 
 git push origin main -f
