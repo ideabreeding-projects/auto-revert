cbe3b5aee6f3a8f2bf62cf6af88089dc9a656e6a
hyseo492 <41456138+hyseo492@users.noreply.github.com>
2024-11-14 11:02:34 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,15 +11,17 @@ REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 
 URI=https://api.github.com
 API_HEADER="X-GitHub-Api-Version: 2022-11-28"
-AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
+AUTH_HEADER="Authorization: Bearer $GITHUB_TOKEN"
 
-commit_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/$REPO_PULLNAME/commits/main")
+echo "AUTH_HEADER=${AUTH_HEADER}"
+
+commit_resp=$(curl -H "${AUTH_HEADER}" -H "$API_HEADER" "${URI}/repos/$REPO_PULLNAME/commits/main")
 
 echo "COMMIT_RESP=${commit_resp}"
 
-PARENT_SHA=$(echo "$commit_resp" | jq -r .sha)
+CURRENT_SHA=$(echo "$commit_resp" | jq -r .sha)
 
-echo "PARENT_SHA=${PARENT_SHA}"
+echo "PARENT_SHA=${CURRENT_SHA}"
 
 git config --global --add safe.directory /github/workspace
 
